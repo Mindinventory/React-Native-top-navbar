3950e375444cdd72fa288ab2f9ab728ebc609d52
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2021-07-16 16:41:15 +0530

    Initial Commit

---
+++ example/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+// import Header from '../src/Header'
+
+const App = () => {
+  return (
+    <>
+      {/* <Header style={{ backgroundColor: '#009999' }} statusBarBackground='#008080' barStyle='light-content'>
+        <Header.Left style={{ backgroundColor: '#009999', width: "15%" }}>
+          <Text>Left</Text>
+        </Header.Left>
+        <Header.Body style={{ backgroundColor: '#009999', width: "70%" }}>
+          <Text>Header Title</Text>
+        </Header.Body>
+        <Header.Right style={{ backgroundColor: '#009999', flexDirection: 'row', width: '15%' }}>
+          <Text>Right</Text>
+        </Header.Right>
+      </Header> */}
+      <SafeAreaView style={styles.conatiner}>
+        <Text>App component</Text>
+      </SafeAreaView>
+    </>
+
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  conatiner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f3f3f3'
+  }
+})
