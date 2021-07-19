d44b898a475390a0bd023da571b8db63d00ea862
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2021-07-19 14:58:00 +0530

    - Changed nav and status bar styles - Updated image size

--- example/App.js
+++ example/App.js
@@ -6,16 +6,16 @@ import Header from '@mindinventory/rn-top-navbar';
 const App = () => {
   return (
     <>
-      <Header style={{ backgroundColor: '#009999' }} statusBarBackground='#008080' barStyle='light-content'>
-        <Header.Left style={{ backgroundColor: '#009999', width: "15%" }}>          
-          <Icon name='arrow-left' style={{color: '#fff', fontSize: 18}} />
-        </Header.Left>
-        <Header.Body style={{ backgroundColor: '#009999', width: "70%" }}>
+      <Header style={{ backgroundColor: '#ff6666' }} statusBarBackground='#ff6666' barStyle='light-content'>
+        {/* <Header.Left style={{ backgroundColor: '#ff6666', width: "15%" }}>          
+          <Icon name='arrow-left' style={{color: '#fff', fontSize: 18}} />          
+        </Header.Left> */}
+        <Header.Body style={{ backgroundColor: '#ff6666', width: "70%" }}>
           <Text style={{color: '#fff'}}>Header Title</Text>
         </Header.Body>
-        <Header.Right style={{ backgroundColor: '#009999', flexDirection: 'row', width: '15%' }}>
-          <Icon name='bars' style={{color: '#fff', fontSize: 18}} />          
-        </Header.Right>
+        {/* <Header.Right style={{ backgroundColor: '#ff6666', flexDirection: 'row', width: '15%' }}>
+          <Icon name='bars' style={{color: '#fff', fontSize: 18}} />                    
+        </Header.Right> */}
       </Header>
       <SafeAreaView style={styles.conatiner}>
         <Text style={{marginHorizontal: 20, textAlign: 'justify', fontSize: 16}}>This is customizable <Text style={{fontWeight: 'bold'}}>rn-top-navbar</Text> package,
